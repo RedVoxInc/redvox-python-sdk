cc340aaca7b9ee188e4278749ccf1b8d8fc35a6e
achriste <achriste@hawaii.edu>
2018-07-27 13:53:15 -1000

    Working packages.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 with open("requirements.txt", "r") as requirements_file:
     requirements = list(map(lambda line: line.strip(), requirements_file.readlines()))
     setup(name='redvox',
-          version='0.3',
+          version='0.6',
           url='https://bitbucket.org/redvoxhi/redvox-api900-python-reader/src/master/',
           license='Apache',
           author='RedVox',
           author_email='dev@redvoxsound.com',
           description='Library for working with RedVox files.',
-          packages=find_packages(exclude=['tests']),
+          packages=find_packages(include=["redvox", "redvox.api900", "redvox.api900.lib"], exclude=['tests']),
           long_description=open('README.md').read(),
           install_requirements=requirements,
           zip_safe=False)
