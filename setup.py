2363606207d022bd0ead95dc4a444354be305929
achriste <achriste@hawaii.edu>
2018-07-27 11:47:56 -1000

    Include requirements in setup.py from requirements.txt file.

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='redvox',
-      version='0.2',
-      url='https://bitbucket.org/redvoxhi/redvox-api900-python-reader/src/master/',
-      license='Apache',
-      author='RedVox',
-      author_email='dev@redvoxsound.com',
-      description='Library for working with RedVox files.',
-      packages=find_packages(exclude=['tests']),
-      long_description=open('README.md').read(),
-      zip_safe=False)
+with open("requirements.txt", "r") as requirements_file:
+    requirements = requirements_file.read.splitlines()
+    setup(name='redvox',
+          version='0.2',
+          url='https://bitbucket.org/redvoxhi/redvox-api900-python-reader/src/master/',
+          license='Apache',
+          author='RedVox',
+          author_email='dev@redvoxsound.com',
+          description='Library for working with RedVox files.',
+          packages=find_packages(exclude=['tests']),
+          long_description=open('README.md').read(),
+          install_requirements=requirements,
+          zip_safe=False)
