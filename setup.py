bcd83722245b6da628bf0b6e14d0d7f01465cac5
achriste <achriste@hawaii.edu>
2018-07-27 14:08:18 -1000

    1.0.0 release.

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 with open("requirements.txt", "r") as requirements_file:
     requirements = list(map(lambda line: line.strip(), requirements_file.readlines()))
-    setup(name='redvox',
-          version='0.6',
-          url='https://bitbucket.org/redvoxhi/redvox-api900-python-reader/src/master/',
-          license='Apache',
-          author='RedVox',
-          author_email='dev@redvoxsound.com',
-          description='Library for working with RedVox files.',
-          packages=find_packages(include=["redvox", "redvox.api900", "redvox.api900.lib"], exclude=['tests']),
-          long_description=open('README.md').read(),
-          install_requirements=requirements,
-          zip_safe=False)
+
+setup(name='redvox',
+    version='1.0.0',
+    url='https://bitbucket.org/redvoxhi/redvox-api900-python-reader/src/master/',
+    license='Apache',
+    author='RedVox',
+    author_email='dev@redvoxsound.com',
+    description='Library for working with RedVox files.',
+    packages=find_packages(include=["redvox", "redvox.api900", "redvox.api900.lib"], exclude=['tests']),
+    long_description=open('README.md').read(),
+    install_requires=requirements,
+    python_requires=">=3.6")
