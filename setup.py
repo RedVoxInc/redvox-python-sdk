e7e04fe5d3c0d6a3fefa74080ef9f2dfa6bcceca
Anthony <achriste@hawaii.edu>
2019-03-06 09:45:07 -1000

    Fix formatting issues.

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open("requirements.txt", "r") as requirements_file:
     requirements = list(map(lambda line: line.strip(), requirements_file.readlines()))
 
 setup(name=redvox.NAME,
-    version=redvox.VERSION,
-    url='https://bitbucket.org/redvoxhi/redvox-api900-python-reader/src/master/',
-    license='Apache',
-    author='RedVox',
-    author_email='dev@redvoxsound.com',
-    description='Library for working with RedVox files.',
-    packages=find_packages(include=["redvox", "redvox.api900", "redvox.api900.lib"], exclude=['tests']),
-    long_description=open('README.md').read(),
-    install_requires=requirements,
-    python_requires=">=3.6")
+      version=redvox.VERSION,
+      url='https://bitbucket.org/redvoxhi/redvox-api900-python-reader/src/master/',
+      license='Apache',
+      author='RedVox',
+      author_email='dev@redvoxsound.com',
+      description='Library for working with RedVox files.',
+      packages=find_packages(include=["redvox", "redvox.api900", "redvox.api900.lib"], exclude=['tests']),
+      long_description=open('README.md').read(),
+      install_requires=requirements,
+      python_requires=">=3.6")
