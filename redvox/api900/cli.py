25f8fe8e4ce13bb7b2e9450b08ae2020a1a7f687
anthonyjchriste <achriste@hawaii.edu>
2019-03-11 10:00:57 -1000

    Update documentation for CLI.

--- redvox/api900/cli.py
+++ redvox/api900/cli.py
@@ -8,7 +8,16 @@ import redvox.api900.reader as reader
 
 
 def to_json(paths: typing.List[str]):
+    """
+    Given a list of paths of .rdvxz files, convert the .rdvxz files to RedVox API 900 compliant .json files.
+    :param paths: List of paths to .rdvxz files.
+    """
     def _to_json(path: str):
+        """
+        Converts a .rdvxz file at path to json and writes the file to the same directory as the .rdvxz file was read
+        from.
+        :param path: Path to the rdvxz file to convert.
+        """
         pb_packet = reader.read_file(path)
         new_path = path.replace(".rdvxz", ".json")
         print("Converting %s -> %s" % (path, new_path))
@@ -19,7 +28,15 @@ def to_json(paths: typing.List[str]):
 
 
 def to_rdvxz(paths: typing.List[str]):
+    """
+    Given a list of paths of RedVox API 900 compliant .json files, convert the .json files to .rdvxz files.
+    :param paths: List of paths to RedVox API 900 compliant .json files.
+    """
     def _to_rdvxz(path: str):
+        """
+        Converts a RedVox API 900 compliant .json file at path to a .rdvxz file in the same directory.
+        :param path: Path to the RedVox API 900 compliant json file.
+        """
         with open(path, "r") as json_in:
             json = json_in.read()
 
@@ -31,17 +48,22 @@ def to_rdvxz(paths: typing.List[str]):
 
 
 def string(paths: typing.List[str]):
+    """
+    For each path passed in, print out the contents of the .rdvxz file using protobuf's string serialization.
+    :param paths: Paths of .rdvxz files.
+    """
     for path in paths:
         print(" ------------- Contents of %s" % path)
         print(reader.read_file(path))
 
 
+# Entry point to CLI.
 if __name__ == "__main__":
-    import argparse
+    import ARGPARSE
 
-    argparse = argparse.ArgumentParser("api900-cli")
+    ARGPARSE = ARGPARSE.ArgumentParser("api900-cli")
 
-    argparse.add_argument("cmd",
+    ARGPARSE.add_argument("cmd",
                           choices=[
                               "to_json",
                               "to_rdvxz",
@@ -49,15 +71,15 @@ if __name__ == "__main__":
                           ],
                           help="Conversion type.")
 
-    argparse.add_argument("file_paths",
+    ARGPARSE.add_argument("file_paths",
                           help="One or more files to convert.",
                           nargs="+")
 
-    args = argparse.parse_args()
-    paths = args.file_paths
-    if args.cmd == "to_json":
-        to_json(args.file_paths)
-    elif args.cmd == "to_rdvxz":
-        to_rdvxz(args.file_paths)
+    ARGS = ARGPARSE.parse_args()
+    PATHS = ARGS.file_paths
+    if ARGS.cmd == "to_json":
+        to_json(ARGS.file_paths)
+    elif ARGS.cmd == "to_rdvxz":
+        to_rdvxz(ARGS.file_paths)
     else:
-        string(args.file_paths)
+        string(ARGS.file_paths)
