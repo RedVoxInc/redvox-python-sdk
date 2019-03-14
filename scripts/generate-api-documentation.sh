86a95ca7131027de9d0ca3b5f6b08d9dd0942d7c
anthonyjchriste <achriste@hawaii.edu>
2019-03-13 16:22:00 -1000

    Add API docs and update doc generation.

--- scripts/generate-api-documentation.sh
+++ scripts/generate-api-documentation.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 cd ..
-source /home/redvox/venvs/redvox/bin/activate
 python3 -m pydoc -w redvox/api900/reader.py
-mv reader.html docs/reader.api.html
+mv reader.html reader.api.html
