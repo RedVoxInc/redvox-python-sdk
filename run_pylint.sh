53bcc81527495c82e885f9c5281b6cf75e9b26ad
anthonyjchriste <achriste@hawaii.edu>
2020-02-18 10:14:21 -1000

    Bump version, start cleaning up CI.

--- run_pylint.sh
+++ run_pylint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-pylint redvox.cli.cli                       && \
-pylint redvox.cli.conversions               && \
-pylint redvox.cli.data_req                  && \
+if ! [[ -x "$(command -v pylint)" ]]; then
+  echo 'Error: pylint is not installed.' >&2
+  exit 1
+fi
+
 pylint redvox.api900.concat                 && \
 pylint redvox.api900.constants              && \
 pylint redvox.api900.date_time_utils        && \
@@ -14,5 +16,10 @@ pylint redvox.api900.stat_utils             && \
 pylint redvox.api900.summarize              && \
 pylint redvox.api900.types                  && \
 pylint redvox.api900.wrapped_redvox_packet  && \
-pylint redvox.api900.sensors
+pylint redvox.api900.sensors                && \
+pylint redvox.cli.cli                       && \
+pylint redvox.cli.conversions               && \
+pylint redvox.cli.data_req                  && \
+pylint redvox.common.date_time_utils        && \
+
 
