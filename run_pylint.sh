c85ec785490b6a771c87af1c08b71dd217fed887
anthonyjchriste <achriste@hawaii.edu>
2020-02-18 10:29:27 -1000

    Add script for checking types via mypy.

--- run_pylint.sh
+++ run_pylint.sh
@@ -5,6 +5,7 @@ if ! [[ -x "$(command -v pylint)" ]]; then
   exit 1
 fi
 
+pylint redvox.api900.sensors                && \
 pylint redvox.api900.concat                 && \
 pylint redvox.api900.constants              && \
 pylint redvox.api900.date_time_utils        && \
@@ -16,10 +17,9 @@ pylint redvox.api900.stat_utils             && \
 pylint redvox.api900.summarize              && \
 pylint redvox.api900.types                  && \
 pylint redvox.api900.wrapped_redvox_packet  && \
-pylint redvox.api900.sensors                && \
 pylint redvox.cli.cli                       && \
 pylint redvox.cli.conversions               && \
 pylint redvox.cli.data_req                  && \
-pylint redvox.common.date_time_utils
+pylint redvox.common
 
 
