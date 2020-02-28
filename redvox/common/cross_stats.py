cf8534f0198ea7bb9ba5ae188ff9c769b286bd1b
opq <achriste@hawaii.edu>
2020-02-27 15:23:08 -1000

    Linting

--- redvox/common/cross_stats.py
+++ redvox/common/cross_stats.py
@@ -19,39 +19,39 @@ def xcorr_all(sig: np.ndarray, sig_ref: np.ndarray) -> Tuple[np.ndarray, np.ndar
     """
     # Generalized two-sensor cross correlation, including unequal lengths
     # Same as main, but more outputs
-    NX = len(sig)
-    NREF = len(sig_ref)
+    sig_len = len(sig)
+    ref_len = len(sig_ref)
     # Faster as floats
     sig = 1.0 * sig
     sig_ref = 1.0 * sig_ref
-    if NX > NREF:
+    if sig_len > ref_len:
         # Cross Correlation 'full' sums over the dimension of sigX
-        xcorr_indexes = np.arange(1 - NX, NREF)
+        xcorr_indexes = np.arange(1 - sig_len, ref_len)
         xcorr = signal.correlate(sig_ref, sig, mode='full')
         # Normalize
-        xcorr /= np.sqrt(NX * NREF) * sig.std() * sig_ref.std()
+        xcorr /= np.sqrt(sig_len * ref_len) * sig.std() * sig_ref.std()
         xcorr_offset_index = xcorr.argmax()
         xcorr_offset_samples = xcorr_indexes[xcorr_offset_index]
-    elif NX < NREF:
+    elif sig_len < ref_len:
         # Cross Correlation 'full' sums over the dimension of sigY
-        xcorr_indexes = np.arange(1 - NREF, NX)
+        xcorr_indexes = np.arange(1 - ref_len, sig_len)
         xcorr = signal.correlate(sig, sig_ref, mode='full')
         # Normalize
-        xcorr /= np.sqrt(NX * NREF) * sig.std() * sig_ref.std()
+        xcorr /= np.sqrt(sig_len * ref_len) * sig.std() * sig_ref.std()
         xcorr_offset_index = xcorr.argmax()
         # Flip sign
         xcorr_offset_samples = -xcorr_indexes[xcorr_offset_index]
-    elif NX == NREF:
-        # Cross correlation is centered in the middle of the record and has length NX
-        # Fastest, o(NX) and can use FFT solution
-        if NX % 2 == 0:
-            xcorr_indexes = np.arange(-int(NX / 2), int(NX / 2))
+    elif sig_len == ref_len:
+        # Cross correlation is centered in the middle of the record and has length sig_len
+        # Fastest, o(sig_len) and can use FFT solution
+        if sig_len % 2 == 0:
+            xcorr_indexes = np.arange(-int(sig_len / 2), int(sig_len / 2))
         else:
-            xcorr_indexes = np.arange(-int(NX / 2), int(NX / 2) + 1)
+            xcorr_indexes = np.arange(-int(sig_len / 2), int(sig_len / 2) + 1)
 
         xcorr = signal.correlate(sig_ref, sig, mode='same')
         # Normalize
-        xcorr /= np.sqrt(NX * NREF) * sig.std() * sig_ref.std()
+        xcorr /= np.sqrt(sig_len * ref_len) * sig.std() * sig_ref.std()
         xcorr_offset_index = xcorr.argmax()
         xcorr_offset_samples = xcorr_indexes[xcorr_offset_index]
     else:
@@ -80,4 +80,3 @@ def xcorr_main(sig: np.ndarray,
     xcorr_offset_seconds = xcorr_offset_samples / sample_rate_hz
 
     return xcorr_normalized_max, xcorr_offset_samples, xcorr_offset_seconds
-
--- redvox/common/errors.py
+++ redvox/common/errors.py
@@ -4,5 +4,8 @@ This module contains custom error definitions used within this SDK.
 
 
 class RedVoxError(Exception):
+    """
+    This class represents generic RedVox SDK errors.
+    """
     def __init__(self, message: str):
         super().__init__(f"RedVoxError: {message}")
