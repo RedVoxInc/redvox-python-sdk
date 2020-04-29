e941aa07fec5359bb91b211bac740db4eecdfbd6
Tyler <tyler@higp.hawaii.edu>
2020-04-28 16:26:17 -1000

    added cleaner way to specify directory for image testing added test image file

--- redvox/tests/api1000/test_image.py
+++ redvox/tests/api1000/test_image.py
@@ -2,13 +2,14 @@ import os
 import numpy as np
 import unittest
 import redvox.api1000.wrapped_redvox_packet.sensors.image as image
+import redvox.tests as test_utils
 
 
 class TestImage(unittest.TestCase):
     def setUp(self) -> None:
         self.empty_image_sensor: image.Image = image.Image.new()
         self.non_empty_image_sensor: image.Image = image.Image.new()
-        with open('/home/redvox/Development/redvox-api900-python-reader/redvox/tests/test_data/redvox_logo.png', "rb") \
+        with open(test_utils.test_data('redvox_logo.png'), "rb") \
                 as image_in:
             data_as_bytes: bytes = image_in.read()
             self.non_empty_image_sensor.set_samples([data_as_bytes, data_as_bytes])
@@ -17,16 +18,15 @@ class TestImage(unittest.TestCase):
         self.non_empty_image_sensor.get_timestamps().set_timestamps(np.array([1000, 2000]), True)
 
     def test_write_image(self):
-        self.non_empty_image_sensor.write_image(
-            "/home/redvox/Development/redvox-api900-python-reader/redvox/tests/test_data/redvox_logo_out")
-        with open('/home/redvox/Development/redvox-api900-python-reader/redvox/tests/test_data/redvox_logo.png', "rb") \
+        self.non_empty_image_sensor.write_image(test_utils.test_data("redvox_logo_out"))
+        with open(test_utils.test_data('redvox_logo.png'), "rb") \
                 as image_in:
             in_bytes: bytes = image_in.read()
-            with open('/home/redvox/Development/redvox-api900-python-reader/redvox/tests/test_data/redvox_logo_out.png', "rb") \
+            with open(test_utils.test_data('redvox_logo_out.png'), "rb") \
                     as image_out:
                 out_bytes: bytes = image_out.read()
                 self.assertEqual(in_bytes, out_bytes)
-        os.remove('/home/redvox/Development/redvox-api900-python-reader/redvox/tests/test_data/redvox_logo_out.png')
+        os.remove(test_utils.test_data('redvox_logo_out.png'))
 
     def test_validate_image(self):
         error_list = image.validate_image(self.non_empty_image_sensor)
