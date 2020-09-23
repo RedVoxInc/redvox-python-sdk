6d094cf991b722851e2d5775314a47d3492507b0
tyler <tyler@isla.hawaii.edu>
2020-09-22 16:40:28 -1000

    removed developmental test case from data windows

--- redvox/tests/common/test_data_window.py
+++ redvox/tests/common/test_data_window.py
@@ -36,38 +36,38 @@ class DataWindowTest(unittest.TestCase):
         self.assertTrue(test_station.has_location_sensor())
 
 
-class OtherTest(unittest.TestCase):
-    def test_mytest(self):
-        from datetime import datetime, timedelta
-        input_dir = "/Users/tyler/IdeaProjects/redvox-projects/tyler/test_read_data"
-        station_ids = {"1637610011", "1637610015"}
-        # please note that InputSettings is a temporary stand-in for a config file
-        settings = [2020, 7, 11, 19, 56, 25, 60]
-        start_timestamp_s: datetime = dt.datetime_from(settings[0],
-                                                       settings[1],
-                                                       settings[2],
-                                                       settings[3],
-                                                       settings[4],
-                                                       settings[5])
-
-        end_timestamp_s: datetime = start_timestamp_s + timedelta(seconds=settings[6])
-        datawindow = dw.DataWindow(input_directory=input_dir,
-                                   station_ids=station_ids,
-                                   start_datetime=start_timestamp_s,
-                                   end_datetime=end_timestamp_s)
-        for station in datawindow.stations.get_all_stations():
-            print(f"station_id: {station.station_metadata.station_id}")
-            if station.has_audio_data():
-                print(f"AUDIO DATA:\n----------------------------\n"
-                      f"mic sample rate: {station.audio_sensor().sample_rate}\n"
-                      f"is mic sample rate constant: {station.audio_sensor().is_sample_rate_fixed}\n"
-                      f"the data as an ndarray: {station.audio_sensor().samples()}\n"
-                      f"the data timestamps: {station.audio_sensor().data_timestamps()}\n"
-                      f"the first data timestamp: {station.audio_sensor().first_data_timestamp()}\n"
-                      f"the last data timestamp: {station.audio_sensor().last_data_timestamp()}\n"
-                      f"the number of data samples: {station.audio_sensor().num_samples()}\n"
-                      f"the duration of the data in seconds: {station.audio_sensor().data_duration_s()}\n"
-                      f"the names of the dataframe columns: {station.audio_sensor().data_fields()}\n")
+# class OtherTest(unittest.TestCase):
+#     def test_mytest(self):
+#         from datetime import datetime, timedelta
+#         input_dir = "/Users/tyler/IdeaProjects/redvox-projects/tyler/test_read_data"
+#         station_ids = {"1637610011", "1637610015"}
+#         # please note that InputSettings is a temporary stand-in for a config file
+#         settings = [2020, 7, 11, 19, 56, 25, 60]
+#         start_timestamp_s: datetime = dt.datetime_from(settings[0],
+#                                                        settings[1],
+#                                                        settings[2],
+#                                                        settings[3],
+#                                                        settings[4],
+#                                                        settings[5])
+#
+#         end_timestamp_s: datetime = start_timestamp_s + timedelta(seconds=settings[6])
+#         datawindow = dw.DataWindow(input_directory=input_dir,
+#                                    station_ids=station_ids,
+#                                    start_datetime=start_timestamp_s,
+#                                    end_datetime=end_timestamp_s)
+#         for station in datawindow.stations.get_all_stations():
+#             print(f"station_id: {station.station_metadata.station_id}")
+#             if station.has_audio_data():
+#                 print(f"AUDIO DATA:\n----------------------------\n"
+#                       f"mic sample rate: {station.audio_sensor().sample_rate}\n"
+#                       f"is mic sample rate constant: {station.audio_sensor().is_sample_rate_fixed}\n"
+#                       f"the data as an ndarray: {station.audio_sensor().samples()}\n"
+#                       f"the data timestamps: {station.audio_sensor().data_timestamps()}\n"
+#                       f"the first data timestamp: {station.audio_sensor().first_data_timestamp()}\n"
+#                       f"the last data timestamp: {station.audio_sensor().last_data_timestamp()}\n"
+#                       f"the number of data samples: {station.audio_sensor().num_samples()}\n"
+#                       f"the duration of the data in seconds: {station.audio_sensor().data_duration_s()}\n"
+#                       f"the names of the dataframe columns: {station.audio_sensor().data_fields()}\n")
         #     if station.has_location_data():
         #         print(f"LOCATION DATA:\n----------------------------\n"
         #               f"location sample rate: {station.location_sensor().sample_rate}\n"
