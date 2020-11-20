cbc5e20e321e7511b865c6abeb3bfb72fe27c6d2
opq <achriste@hawaii.edu>
2020-11-20 13:57:26 -1000

    Linting, docs, and cleanup.

--- redvox/cloud/errors.py
+++ redvox/cloud/errors.py
@@ -1,13 +1,27 @@
+"""
+Custom defined RedVox Cloud derived errors.
+"""
+
+
 class CloudApiError(Exception):
+    """
+    A generic Cloud API error.
+    """
     def __init__(self, message: str = ""):
         super().__init__(message)
 
 
 class AuthenticationError(CloudApiError):
+    """
+    A cloud authentication error.
+    """
     def __init__(self):
         super().__init__("Authentication Error")
 
 
 class ApiConnectionError(CloudApiError):
+    """
+    A connection error.
+    """
     def __init__(self, message: str = ""):
         super().__init__(f"ApiConnectionError: {message}")
