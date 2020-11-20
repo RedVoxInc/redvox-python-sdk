28d3a30bf9ad031ae14fe1ceebf69b831d9c4405
Anthony Christe <achriste@redvoxsound.com>
2020-11-19 15:30:42 -1000

    Updates

--- redvox/common/runtime_properties.py
+++ redvox/common/runtime_properties.py
@@ -5,5 +5,20 @@ aspects of the SDK.
 
 from datetime import timedelta
 
-QUERY_TIME_START_BUF: timedelta = timedelta(minutes=2.0)
-QUERY_TIME_END_BUF: timedelta = timedelta(minutes=2.0)
+from redvox.api1000.common.common import check_type
+
+__QUERY_TIME_START_BUF: timedelta = timedelta(minutes=2.0)
+__QUERY_TIME_END_BUF: timedelta = timedelta(minutes=2.0)
+
+
+def get_query_time_start_buf() -> timedelta:
+    return __QUERY_TIME_START_BUF
+
+
+def get_query_time_end_buf() -> timedelta:
+    return __QUERY_TIME_END_BUF
+
+
+def set_query_time_start_buf(query_time_start_buf: timedelta) -> None:
+    check_type(query_time_start_buf, [timedelta])
+    __QUERY_TIME_START_BUF = query_time_start_buf
