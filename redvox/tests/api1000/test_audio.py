12988e13f452bf9c12c222ffc5db221f554471a8
opq <achriste@hawaii.edu>
2020-11-23 16:34:45 -1000

    Test setting of samples.

--- redvox/tests/api1000/test_audio.py
+++ redvox/tests/api1000/test_audio.py
@@ -20,3 +20,12 @@ class TestAudio(unittest.TestCase):
         self.assertEqual(error_list, [])
         error_list = microphone.validate_audio(self.empty_microphone_sensor)
         self.assertNotEqual(error_list, [])
+
+    def test_set_samples(self):
+        sample_payload: common.SamplePayload = common.SamplePayload.new()
+        sample_payload.append_values(np.arange(0, 10))
+        self.empty_microphone_sensor.set_samples(sample_payload)
+
+        self.assertIsNotNone(self.empty_microphone_sensor.get_samples())
+        self.assertEqual(self.empty_microphone_sensor.get_samples().get_values_count(), 10)
+        self.assertEqual(list(self.empty_microphone_sensor.get_samples().get_values()), list(range(10)))
