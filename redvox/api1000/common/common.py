f34d0e6040c6bc5670359eff79a1218e7d4a1720
tyler <tyler@isla.hawaii.edu>
2021-01-04 16:14:50 -1000

    fix for mean and std dev of sample intervals less than 2 elements long

--- redvox/api1000/common/common.py
+++ redvox/api1000/common/common.py
@@ -354,6 +354,8 @@ def sampling_rate_statistics(timestamps: np.ndarray) -> Tuple[float, float]:
     :return: A tuple containing (mean_sample_rate, stdev_sample_rate)
     """
     sample_interval: np.ndarray = np.diff(timestamps)
+    if len(sample_interval) < 2:
+        return 0.0, 0.0
     mean_sample_interval: float = sample_interval.mean()
     stdev_sample_interval: float = sample_interval.std()
 
