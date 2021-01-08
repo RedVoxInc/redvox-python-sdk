824ba3190998e7666c63063011e413a91529922b
tyler <tyler@isla.hawaii.edu>
2021-01-08 11:30:35 -1000

    api reader updates

--- redvox/common/api_reader.py
+++ redvox/common/api_reader.py
@@ -7,50 +7,23 @@ import os
 from glob import glob
 from typing import List
 
-from redvox.api1000 import io_raw as apim_io_raw
 from redvox.api1000.wrapped_redvox_packet.wrapped_packet import WrappedRedvoxPacketM
 from redvox.api900 import reader as api900_io
 from redvox.common import api_conversions as ac
+from redvox.common.io import types as io_types, lib as io_lib
 
 
-def read_all_in_unstructured_dir(base_dir: str) -> apim_io_raw.ReadResult:
-    converted_data: List[WrappedRedvoxPacketM] = []
-    api_900_data = api900_io.read_rdvxz_file_range(base_dir, structured_layout=False, concat_continuous_segments=False)
-    for ids in api_900_data.keys():
-        for packet in api_900_data[ids]:
-            converted_data.append(ac.convert_api_900_to_1000(packet))
+def read_all_in_dir(base_dir: str, io_filter: io_types.ReadFilter, structured: bool = False) -> io_types.ReadResult:
+    if structured:
+        paths = io_lib.index_structured(base_dir, io_filter)
+    else:
+        paths = io_lib.index_unstructured(base_dir, io_filter)
+    api_m_data: List[WrappedRedvoxPacketM] = []
+    for path in paths:
+        if path.extension == ".rdvxz":
+            data = api900_io.wrap(api900_io.read_file(path.full_path, True))
+            api_m_data.append(ac.convert_api_900_to_1000(data))
+        elif path.extension == ".rdvxm":
+            api_m_data.append(WrappedRedvoxPacketM.from_compressed_path(path.full_path))
 
-    api_m_filter = apim_io_raw.ReadFilter()
-    pattern: str = os.path.join(base_dir, f"*{api_m_filter.extension}")
-    paths: List[str] = glob(os.path.join(base_dir, pattern))
-    paths = list(filter(api_m_filter.filter_path, paths))
-    api_m_data: List[WrappedRedvoxPacketM] = apim_io_raw.__deserialize_paths(paths)
-
-    # combine api900 and api m
-    api_m_data.extend(converted_data)
-
-    return apim_io_raw.ReadResult.from_packets(api_m_data)
-
-
-def read_all_in_structured_dir_io_raw(base_dir: str) -> apim_io_raw.ReadResult:
-    converted_data: List[WrappedRedvoxPacketM] = []
-    api_900_data = api900_io.read_rdvxz_file_range(os.path.join(base_dir, "api900"),
-                                                   structured_layout=True, concat_continuous_segments=False)
-    for ids in api_900_data.keys():
-        for packet in api_900_data[ids]:
-            converted_data.append(ac.convert_api_900_to_1000(packet))
-
-    # apim_io_raw.check_type(base_dir, [str])
-    # api_900_filter = apim_io_raw.ReadFilter(extension=".rdvxz")
-    # apim_io_raw.check_type(api_900_filter, [apim_io_raw.ReadFilter])
-    # paths: List[str] = __parse_structured_layout_rdvxz(os.path.join(base_dir, "api900"), api_900_filter)
-    # api_900_data: List[WrappedRedvoxPacketM] = apim_io_raw.__deserialize_paths(paths)
-
-    api_m_filter = apim_io_raw.ReadFilter()
-    paths: List[str] = apim_io_raw.__parse_structured_layout(os.path.join(base_dir, "api1000"), api_m_filter)
-    api_m_data: List[WrappedRedvoxPacketM] = apim_io_raw.__deserialize_paths(paths)
-
-    # combine api900 and api m
-    api_m_data.extend(converted_data)
-
-    return apim_io_raw.ReadResult.from_packets(api_m_data)
+    return io_types.ReadResult.from_packets(api_m_data)
--- redvox/tests/common/test_api_reader.py
+++ redvox/tests/common/test_api_reader.py
@@ -5,11 +5,13 @@ import unittest
 
 import redvox.tests as tests
 from redvox.common import api_reader
+from redvox.common.io import types as io_types
 
 
 class ApiReaderTest(unittest.TestCase):
     def setUp(self):
         self.input_dir = tests.APIX_READER_TEST_DATA_DIR
+        self.filter = io_types.ReadFilter(extensions={".rdvxm", ".rdvxz"})
 
     # def test_api_reader_read_all_in_dir(self):
     #     result = api_reader.read_all_in_dir(self.input_dir, structured_layout=True)
@@ -20,13 +22,13 @@ class ApiReaderTest(unittest.TestCase):
     #     self.assertEqual(len(result.get_by_id("1637620010")), 3)
 
     def test_read_all_in_unstructured_dir(self):
-        result = api_reader.read_all_in_unstructured_dir(self.input_dir)
+        result = api_reader.read_all_in_dir(self.input_dir, self.filter)
         self.assertEqual(len(result.station_id_uuid_to_packets.keys()), 2)
         self.assertEqual(len(result.get_packets_for_station_id("0000000001")), 1)
         self.assertEqual(len(result.get_packets_for_station_id("1637650010")), 1)
 
     def test_read_all_in_structured_dir_io_raw(self):
-        result = api_reader.read_all_in_structured_dir_io_raw(self.input_dir)
+        result = api_reader.read_all_in_dir(self.input_dir, self.filter, True)
         self.assertEqual(len(result.station_id_uuid_to_packets.keys()), 6)
         self.assertEqual(len(result.get_packets_for_station_id("1637610022")), 3)
         self.assertEqual(len(result.get_packets_for_station_id("1637620009")), 3)
