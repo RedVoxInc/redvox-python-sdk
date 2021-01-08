c114ec116cf815b300b2068da4327789fa0a7be7
Anthony Christe <achriste@redvoxsound.com>
2021-01-08 10:47:13 -1000

    Fix imports in versioning since the ApiVersion class got moved to io/types.

--- redvox/common/versioning.py
+++ redvox/common/versioning.py
@@ -1,10 +1,14 @@
 from enum import Enum
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from redvox.common.io.types import ApiVersion
 
 LZ4_MAGIC: bytes = b'\x04"M\x18'
 
 
 # noinspection PyTypeChecker
-def check_version_buf(buf: bytes) -> ApiVersion:
+def check_version_buf(buf: bytes) -> 'ApiVersion':
     """
     Attempts to check the API version of a given RedVox buffer by looking for the LZ4 frame header which is only present
     in API M compressed data.
@@ -12,14 +16,14 @@ def check_version_buf(buf: bytes) -> ApiVersion:
     :return: An enum that represents the API version of the file.
     """
     if len(buf) < 4:
-        return ApiVersion.UNKNOWN
+        return 'ApiVersion'.UNKNOWN
     if buf[:4] == LZ4_MAGIC:
-        return ApiVersion.API_1000
-    return ApiVersion.API_900
+        return 'ApiVersion'.API_1000
+    return 'ApiVersion'.API_900
 
 
 # noinspection PyTypeChecker
-def check_version(path: str) -> ApiVersion:
+def check_version(path: str) -> 'ApiVersion':
     """
     Attempts to check the API version of a given RedVox file by looking for the LZ4 frame header which is only present
     in API M compressed data.
