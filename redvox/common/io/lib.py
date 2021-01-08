c0f2f4d4138c614b0a9e2b7883a134597314dad3
opq <achriste@hawaii.edu>
2021-01-08 10:09:34 -1000

    Updates.

--- redvox/common/io/lib.py
+++ redvox/common/io/lib.py
@@ -1,12 +1,12 @@
 from glob import glob
-from typing import Iterator, List
+from typing import Iterator, List, Optional
 import os.path
 
 from redvox.api1000.common.typing import check_type
 from redvox.common.io.types import ReadFilter, PathDescriptor
 
 
-def index_unstructured(base_dir: str, read_filter: ReadFilter = ReadFilter()) -> Iterator[PathDescriptor]:
+def index_unstructured(base_dir: str, read_filter: ReadFilter = ReadFilter()) -> List[PathDescriptor]:
     """
     Returns the list of file paths that match the given filter for unstructured data.
     :param base_dir: Directory containing unstructured data.
@@ -15,6 +15,16 @@ def index_unstructured(base_dir: str, read_filter: ReadFilter = ReadFilter()) ->
     """
     check_type(base_dir, [str])
     check_type(read_filter, [ReadFilter])
-    pattern: str = os.path.join(base_dir, f"*{read_filter.extension}")
-    paths: List[str] = glob(os.path.join(base_dir, pattern))
-    return filter(read_filter.apply, paths)
+
+    path_descriptors: List[PathDescriptor] = []
+
+    extension: str
+    for extension in read_filter.extensions:
+        pass
+
+    # pattern: str = os.path.join(base_dir, f"*{read_filter.extension}")
+    # paths: List[str] = glob(os.path.join(base_dir, pattern))
+    # path_descriptors: Iterator[Optional[PathDescriptor]] = map()
+    # return filter(read_filter.apply, paths)
+
+    return path_descriptors
