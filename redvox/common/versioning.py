1d8331679cfa0365f3ce7d27566776ae68511ea9
opq <achriste@hawaii.edu>
2021-01-07 15:14:54 -1000

    Cleanup version checking.

--- redvox/common/versioning.py
+++ redvox/common/versioning.py
@@ -9,6 +9,22 @@ class ApiVersion(Enum):
     """
     API_900: str = "API_900"
     API_1000: str = "API_1000"
+    UNKNOWN: str = "UNKNOWN"
+
+
+# noinspection PyTypeChecker
+def check_version_buf(buf: bytes) -> ApiVersion:
+    """
+    Attempts to check the API version of a given RedVox buffer by looking for the LZ4 frame header which is only present
+    in API M compressed data.
+    :param buf: Buffer of RedVox data.
+    :return: An enum that represents the API version of the file.
+    """
+    if len(buf) < 4:
+        return ApiVersion.UNKNOWN
+    if buf[:4] == LZ4_MAGIC:
+        return ApiVersion.API_1000
+    return ApiVersion.API_900
 
 
 # noinspection PyTypeChecker
@@ -20,7 +36,5 @@ def check_version(path: str) -> ApiVersion:
     :return: An enum that represents the API version of the file.
     """
     with open(path, "rb") as fin:
-        header = fin.read(4)
-        if header == LZ4_MAGIC:
-            return ApiVersion.API_1000
-        return ApiVersion.API_900
+        return check_version_buf(fin.read(4))
+
