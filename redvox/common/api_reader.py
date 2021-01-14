2aac5c8ec67dd285385c9ff7b3ded7fa93da60d1
tyler <tyler@isla.hawaii.edu>
2021-01-14 11:20:49 -1000

    updated with latest io.py changes api_reader starting to become more like data_window

--- redvox/common/api_reader.py
+++ redvox/common/api_reader.py
@@ -3,7 +3,8 @@ Read Redvox data from a single directory
 Data files can be either API 900 or API 1000 data formats
 The ReadResult object converts api900 data into api 1000 format
 """
-from typing import List, Optional
+from typing import List, Optional, Set
+from datetime import datetime, timedelta
 
 from redvox.api1000.wrapped_redvox_packet.wrapped_packet import WrappedRedvoxPacketM
 from redvox.api900 import reader as api900_io
@@ -11,12 +12,68 @@ from redvox.common import api_conversions as ac
 from redvox.common import io
 
 
+DEFAULT_GAP_TIME_S: float = 0.25        # default length of a gap in seconds
+
+
 class ApiReader:
-    def __init__(self, read_filter: io.ReadFilter, base_dir: str, structured_dir: bool = False):
-        self.filter = read_filter
+    """
+    Reads data from api 900 or api 1000 format, converting all data read into WrappedRedvoxPacketM for
+        ease of comparison and use.
+    Properties:
+        filter: io.ReadFilter with the station ids, start and end time, start and end time padding, and
+                types of files to read
+        base_dir: str of the directory containing all the files to read
+        structured_dir: bool, if True, the base_dir contains a specific directory structure used by the
+                        respective api formats.  If False, base_dir only has the data files.  Default False.
+        files_index: io.Index of the files that match the filter that are in base_dir
+        index_summary: io.IndexSummary of the filtered data
+        gap_time_s: int, minimum amount of seconds between data points to be considered a gap.  Default GAP_TIME_S
+        apply_correction: bool, if True, apply timing correction values based on the data in the packets.  Default True
+        debug: bool, if True, output additional information during function execution.  Default False.
+    """
+    def __init__(self, base_dir: str, structured_dir: bool = False, start_dt: Optional[datetime] = None,
+                 end_dt: Optional[datetime] = None, start_dt_buf: Optional[timedelta] = None,
+                 end_dt_buf: Optional[timedelta] = None, station_ids: Optional[Set[str]] = None,
+                 extensions: Optional[Set[str]] = None, api_versions: Optional[Set[io.ApiVersion]] = None,
+                 gap_time_s: int = DEFAULT_GAP_TIME_S, apply_timing_correction: bool = True, debug: bool = False):
+        """
+        Initialize the ApiReader object
+        :param base_dir: directory containing the files to read
+        :param structured_dir: if True, base_dir contains a specific directory structure used by the respective
+                                api formats.  If False, base_dir only has the data files.  Default False.
+        :param start_dt: optional starting datetime to filter from.  If None, get all data in base_dir.  Default None
+        :param end_dt: optional ending datetime to filter from.  If None, get all data in base_dir.  Default None
+        :param start_dt_buf: optional timedelta buffer to combine with start_dt.
+                                If None, use the default in io.ReadFilter.  Default None
+        :param end_dt_buf: optional timedelta buffer to combine with end_dt.
+                            If None, use the default in io.ReadFilter.  Default None
+        :param station_ids: optional set of station_ids to filter on.  If None, get all data in base_dir.  Default None
+        :param extensions: optional set of file extensions to filter on.  If None, get all data in base_dir.
+                            Default None
+        :param api_versions: optional set of api versions to filter on.  If None, get all data in base_dir.
+                            Default None
+        :param gap_time_s: minimum number of seconds between data points to be considered a gap.
+                            Default defined as DEFAULT_GAP_TIME_S
+        :param apply_timing_correction: if True, adjust timestamps in the resulting WrappedRedvoxPacketM objects
+                                        based on the packets' data.  Default True
+        :param debug: if True, output additional statements during function execution.  Default False.
+        """
+        self.filter = io.ReadFilter(start_dt=start_dt, end_dt=end_dt, station_ids=station_ids)
+        if start_dt_buf:
+            self.filter = self.filter.with_start_dt_buf(start_dt_buf)
+        if end_dt_buf:
+            self.filter = self.filter.with_end_dt_buf(end_dt_buf)
+        if extensions:
+            self.filter = self.filter.with_extensions(extensions)
+        if api_versions:
+            self.filter = self.filter.with_api_versions(api_versions)
         self.base_dir = base_dir
         self.structured_dir = structured_dir
+        self.gap_time_s = gap_time_s
+        self.apply_correction = apply_timing_correction
+        self.debug = debug
         self.files_index = self.get_all_files()
+        self.index_summary = io.IndexSummary.from_index(self.files_index)
 
     def get_all_files(self) -> io.Index:
         """
--- redvox/tests/common/test_api_reader.py
+++ redvox/tests/common/test_api_reader.py
@@ -7,20 +7,19 @@ import os
 
 import redvox.tests as tests
 from redvox.common import date_time_utils as dtu
-from redvox.common import api_reader, io
+from redvox.common import api_reader
 
 
 class ApiReaderTest(unittest.TestCase):
-    def setUp(self):
-        self.input_dir = tests.APIX_READER_TEST_DATA_DIR
-        self.filter = io.ReadFilter(extensions={".rdvxm", ".rdvxz"})
-        self.api_900_filter = io.ReadFilter(extensions={".rdvxz"})
-        self.api_1000_filter = io.ReadFilter(extensions={".rdvxm"})
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.input_dir = tests.APIX_READER_TEST_DATA_DIR
+        cls.api_900_extensions = {".rdvxz"}
+        cls.api_1000_extensions = {".rdvxm"}
 
     def test_read_all_in_dir_start_time(self):
-        time_filter = io.ReadFilter(extensions={".rdvxm", ".rdvxz"}, start_dt_buf=timedelta(seconds=10),
-                                    start_dt=dtu.datetime_from_epoch_microseconds_utc(1609984850000000))
-        reader = api_reader.ApiReader(time_filter, self.input_dir, True)
+        reader = api_reader.ApiReader(self.input_dir, True, start_dt_buf=timedelta(seconds=10),
+                                      start_dt=dtu.datetime_from_epoch_microseconds_utc(1609984850000000))
         result = reader.read_files()
         self.assertEqual(len(result), 12)
         result_by_id = api_reader.get_data_by_id(result, "1637610021")
@@ -30,30 +29,30 @@ class ApiReaderTest(unittest.TestCase):
         result_by_id = api_reader.get_data_by_id(result, "1637110703")
         self.assertEqual(len(result_by_id), 2)
 
-    def test_read_all_in_dir_station_ids_fail(self):
-        ids_filter = io.ReadFilter(extensions={".rdvxm", ".rdvxz"}, station_ids={"1637610021"})
+    def test_read_all_in_dir_station_ids_no_match(self):
         api1000_dir = os.path.join(tests.APIX_READER_TEST_DATA_DIR, "api1000")
-        reader = api_reader.ApiReader(ids_filter, api1000_dir, True)
+        reader = api_reader.ApiReader(api1000_dir, True, station_ids={"1637610021"})
         result = reader.read_files()
         self.assertEqual(len(result), 0)
+        result_by_id = api_reader.get_data_by_id(result, "1637610021")
+        self.assertIsNone(result_by_id)
 
     def test_read_all_in_dir_station_ids(self):
-        ids_filter = io.ReadFilter(extensions={".rdvxm", ".rdvxz"}, station_ids={"1637610021", "0000000001"})
-        reader = api_reader.ApiReader(ids_filter, self.input_dir, True)
+        reader = api_reader.ApiReader(self.input_dir, True, station_ids={"1637610021", "0000000001"})
         result = reader.read_files()
         self.assertEqual(len(result), 3)
         result_by_id = api_reader.get_data_by_id(result, "1637610021")
         self.assertEqual(len(result_by_id), 3)
 
     def test_read_all_api900_in_unstructured_dir(self):
-        reader = api_reader.ApiReader(self.api_900_filter, self.input_dir)
+        reader = api_reader.ApiReader(self.input_dir, extensions=self.api_900_extensions)
         result = reader.read_files()
         self.assertEqual(len(result), 1)
         result_by_id = api_reader.get_data_by_id(result, "1637650010")
         self.assertEqual(len(result_by_id), 1)
 
-    def test_read_all_api900_in_structured_dir_io_raw(self):
-        reader = api_reader.ApiReader(self.api_900_filter, self.input_dir, True)
+    def test_read_all_api900_in_structured_dir(self):
+        reader = api_reader.ApiReader(self.input_dir, True, extensions=self.api_900_extensions)
         result = reader.read_files()
         self.assertEqual(len(result), 9)
         result_by_id = api_reader.get_data_by_id(result, "1637610021")
@@ -64,14 +63,14 @@ class ApiReaderTest(unittest.TestCase):
         self.assertEqual(len(result_by_id), 3)
 
     def test_read_all_api1000_in_unstructured_dir(self):
-        reader = api_reader.ApiReader(self.api_1000_filter, self.input_dir)
+        reader = api_reader.ApiReader(self.input_dir, extensions=self.api_1000_extensions)
         result = reader.read_files()
         self.assertEqual(len(result), 1)
         result_by_id = api_reader.get_data_by_id(result, "0000000001")
         self.assertEqual(len(result_by_id), 1)
 
-    def test_read_all_api1000_in_structured_dir_io_raw(self):
-        reader = api_reader.ApiReader(self.api_1000_filter, self.input_dir, True)
+    def test_read_all_api1000_in_structured_dir(self):
+        reader = api_reader.ApiReader(self.input_dir, True, extensions=self.api_1000_extensions)
         result = reader.read_files()
         self.assertEqual(len(result), 9)
         result_by_id = api_reader.get_data_by_id(result, "1637610022")
@@ -82,7 +81,7 @@ class ApiReaderTest(unittest.TestCase):
         self.assertEqual(len(result_by_id), 3)
 
     def test_read_all_in_unstructured_dir(self):
-        reader = api_reader.ApiReader(self.filter, self.input_dir)
+        reader = api_reader.ApiReader(self.input_dir)
         result = reader.read_files()
         self.assertEqual(len(result), 2)
         result_by_id = api_reader.get_data_by_id(result, "0000000001")
@@ -91,7 +90,7 @@ class ApiReaderTest(unittest.TestCase):
         self.assertEqual(len(result_by_id), 1)
 
     def test_read_all_in_structured_dir_io_raw(self):
-        reader = api_reader.ApiReader(self.filter, self.input_dir, True)
+        reader = api_reader.ApiReader(self.input_dir, True)
         result = reader.read_files()
         self.assertEqual(len(result), 18)
         result_by_id = api_reader.get_data_by_id(result, "1637610022")
@@ -107,8 +106,7 @@ class ApiReaderTest(unittest.TestCase):
         filter_ids = ["1637610021", "1637610022", "1637620009", "1637620010", "1637650010"]
         final_result = []
         for f_id in filter_ids:
-            read_filter = io.ReadFilter(station_ids={f_id})
-            reader = api_reader.ApiReader(read_filter, self.input_dir, True)
+            reader = api_reader.ApiReader(self.input_dir, True, station_ids={f_id})
             result = reader.read_files()
             if len(result) == 0:
                 self.assertTrue("1637650010" in reader.filter.station_ids)
