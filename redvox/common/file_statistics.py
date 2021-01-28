251c49ef8049d7684953867e501d1f84bc47f916
opq <achriste@hawaii.edu>
2021-01-28 13:15:36 -1000

    Add serial and parallel stats extraction to the SDK.

--- redvox/common/file_statistics.py
+++ redvox/common/file_statistics.py
@@ -2,9 +2,32 @@
 This module provides utility functions for determining statistics of well structured RedVox data.
 """
 
-from typing import Tuple, Union
+from dataclasses import dataclass
+from datetime import datetime, timedelta
+from typing import Any, Callable, Iterator, List, Optional, Tuple, TYPE_CHECKING, Union
+import multiprocessing
 
 import numpy as np
+
+# noinspection Mypy
+if TYPE_CHECKING:
+    from redvox.api1000.wrapped_redvox_packet.sensors.audio import Audio
+    from redvox.api1000.wrapped_redvox_packet.sensors.location import Location
+    from redvox.api1000.wrapped_redvox_packet.sensors.sensors import Sensors
+    from redvox.api1000.wrapped_redvox_packet.station_information import (
+        StationInformation,
+    )
+    from redvox.api1000.wrapped_redvox_packet.timing_information import (
+        TimingInformation,
+    )
+    from redvox.api1000.wrapped_redvox_packet.wrapped_packet import WrappedRedvoxPacketM
+    from redvox.api900.wrapped_redvox_packet import WrappedRedvoxPacket
+
+# noinspection Mypy
+from redvox.common.date_time_utils import datetime_from_epoch_microseconds_utc as us2dt
+
+# noinspection Mypy
+import redvox.common.io as io
 
 SAMPLE_RATE_HZ: np.ndarray = np.array(
     [80, 800, 8000, 16000]
@@ -70,3 +93,179 @@ def get_duration_seconds_from_sample_rate(sample_rate: Union[float, int]) -> flo
         raise ValueError(
             f"Unknown sample rate {sample_rate} given to compute duration!"
         ) from ex
+
+
+def _map_opt(
+    opt: Optional[Any], apply: Callable[[Optional[Any]], Optional[Any]]
+) -> Optional[Any]:
+    """
+    Maps an optional with the given function. If the optional is None, None is returned.
+    :param opt: The optional to map.
+    :param apply: The function to apply to the optional if a value is present.
+    :return: The mapped value.
+    """
+    if opt is None:
+        return None
+
+    return apply(opt)
+
+
+def _partition_list(lst: List[Any], chunks: int) -> List[Any]:
+    """
+    Partitions a list into k "chunks" of approximately equal length.
+    Adapted from: Adopted from: https://stackoverflow.com/questions/2130016/splitting-a-list-into-n-parts-of-approximately-equal-length/37414115#37414115
+    :param lst:
+    :param chunks:
+    :return:
+    """
+    n: int = len(lst)
+    k: int = chunks
+    return [
+        lst[i * (n // k) + min(i, n % k) : (i + 1) * (n // k) + min(i + 1, n % k)]
+        for i in range(k)
+    ]
+
+
+@dataclass
+class GpsTimestamp:
+    """
+    Represents timestamp pairings in API 1000 data.
+    """
+
+    mach_ts: datetime
+    gps_ts: Optional[datetime]
+
+
+@dataclass
+class StationStat:
+    """
+    A collection of station fields for a given API 900 or API 1000 packet.
+    These are used for timing correction and gap detection.
+    """
+
+    station_id: str
+    station_uuid: str
+    app_start: Optional[datetime]
+    packet_start_ts: datetime
+    server_recv_ts: datetime
+    gps_timestamps: Optional[List[GpsTimestamp]]
+    latency: Optional[float]
+    offset: Optional[float]
+    sample_rate_hz: Optional[float]
+    packet_duration: timedelta
+
+    @staticmethod
+    def from_api_900(packet: "WrappedRedvoxPacket") -> "StationStat":
+        """
+        Extracts the required fields from an API 900 packet.
+        :param packet: API 900 packet to extract fields from.
+        :return: An instance of StationStat.
+        """
+        mtz: Optional[float] = packet.mach_time_zero()
+
+        # noinspection Mypy
+        return StationStat(
+            packet.redvox_id(),
+            packet.uuid(),
+            _map_opt(mtz, us2dt),
+            us2dt(packet.app_file_start_timestamp_machine()),
+            us2dt(packet.server_timestamp_epoch_microseconds_utc()),
+            None,
+            packet.best_latency(),
+            packet.best_offset(),
+            packet.microphone_sensor().sample_rate_hz(),
+            timedelta(seconds=packet.duration_s()),
+        )
+
+    @staticmethod
+    def from_api_1000(packet: "WrappedRedvoxPacketM") -> "StationStat":
+        """
+        Extracts the required fields from an API 1000 packet.
+        :param packet: API 1000 packet to extract fields from.
+        :return: An instance of StationStat.
+        """
+        station_info: "StationInformation" = packet.get_station_information()
+        timing_info: "TimingInformation" = packet.get_timing_information()
+        sensors: "Sensors" = packet.get_sensors()
+        location_sensor: Optional["Location"] = sensors.get_location()
+        audio_sensor: Optional["Audio"] = sensors.get_audio()
+
+        # Optionally extract the GPS timestamps if the location sensor is available
+        gps_timestamps: Optional[List[GpsTimestamp]] = None
+        if location_sensor is not None:
+            gps_timestamps = []
+            _gps_timestamps = location_sensor.get_timestamps_gps().get_timestamps()
+            _gps_timestamps_len = len(_gps_timestamps)
+            for i, ts in enumerate(location_sensor.get_timestamps().get_timestamps()):
+                # A GPS timestamp isn't always present in the location sensor. We can handle that here.
+                gps_ts: Optional[datetime] = (
+                    us2dt(_gps_timestamps[i]) if i < _gps_timestamps_len else None
+                )
+                gps_timestamps.append(GpsTimestamp(us2dt(ts), gps_ts))
+
+        return StationStat(
+            station_info.get_id(),
+            station_info.get_uuid(),
+            us2dt(timing_info.get_app_start_mach_timestamp()),
+            us2dt(timing_info.get_packet_start_mach_timestamp()),
+            us2dt(timing_info.get_server_acquisition_arrival_timestamp()),
+            gps_timestamps,
+            timing_info.get_best_latency(),
+            timing_info.get_best_offset(),
+            _map_opt(audio_sensor, lambda sensor: sensor.get_sample_rate()),
+            packet.get_packet_duration(),
+        )
+
+
+# noinspection PyTypeChecker
+def extract_stats_serial(index: io.Index) -> List[StationStat]:
+    """
+    Extracts StationStat information from packets stored in the provided index.
+    :param index: Index of packets to extract information from.
+    :return: A list of StationStat objects.
+    """
+    # noinspection Mypy
+    stats_900: Iterator[StationStat] = map(
+        StationStat.from_api_900,
+        index.stream(io.ReadFilter(api_versions={io.ApiVersion.API_900})),
+    )
+    # noinspection Mypy
+    stats_1000: Iterator[StationStat] = map(
+        StationStat.from_api_1000,
+        index.stream(io.ReadFilter(api_versions={io.ApiVersion.API_1000})),
+    )
+    return list(stats_900) + list(stats_1000)
+
+
+def extract_stats_parallel(index: io.Index) -> List[StationStat]:
+    """
+    Extracts StationStat information in parallel from packets stored in the provided index.
+    :param index: Index of packets to extract information from.
+    :return: A list of StationStat objects.
+    """
+    # Partition the index entries by number of cores
+    num_cores: int = multiprocessing.cpu_count()
+    partitioned: List[List[io.IndexEntry]] = _partition_list(index.entries, num_cores)
+    indices: List[io.Index] = list(map(lambda entries: io.Index(entries), partitioned))
+
+    # Run da buggahs in parallel
+    pool = multiprocessing.Pool(processes=num_cores)
+    nested: List[List[StationStat]] = pool.map(extract_stats_serial, indices)
+    return [item for sublist in nested for item in sublist]
+
+
+def extract_stats(
+    index: io.Index, min_len_for_parallel: int = 128
+) -> List[StationStat]:
+    """
+    Extracts StationStat information from packets stored in the provided index.
+    :param index: Index of packets to extract information from.
+    :param min_len_for_parallel: When the index contains more than this number of entries, the values will be read in
+                                 parallel, otherwise they are read in serial.
+    :return: A list of StationStat objects.
+    """
+    if len(index.entries) >= min_len_for_parallel:
+        return extract_stats_parallel(index)
+    else:
+        return extract_stats_serial(index)
+
