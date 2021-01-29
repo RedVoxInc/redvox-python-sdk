6e574579967a5deb883cc95ad47899913106c3d0
opq <achriste@hawaii.edu>
2021-01-28 16:57:45 -1000

    updates

--- redvox/cli/data_req.py
+++ redvox/cli/data_req.py
@@ -5,54 +5,42 @@ This module provides methods and classes for requesting bulk RedVox data.
 import logging
 from typing import List, TYPE_CHECKING
 
-import redvox.cloud.api as cloud_api
 import redvox.cloud.client as cloud_client
 import redvox.cloud.data_api as data_api
 import redvox.cloud.data_client as data_client
 
 if TYPE_CHECKING:
     from redvox.cloud.data_api import DataRangeReqType
+    from redvox.cloud.config import RedVoxConfig
 
 # pylint: disable=C0103
 log = logging.getLogger(__name__)
 
 
-def make_data_req(out_dir: str,
-                  protocol: str,
-                  host: str,
-                  port: int,
-                  email: str,
-                  password: str,
-                  req_start_s: int,
-                  req_end_s: int,
-                  redvox_ids: List[str],
-                  api_type: 'DataRangeReqType',
-                  retries: int,
-                  secret_token: str) -> bool:
+def make_data_req(
+    out_dir: str,
+    redvox_config: "RedVoxConfig",
+    req_start_s: int,
+    req_end_s: int,
+    redvox_ids: List[str],
+    api_type: "DataRangeReqType",
+    retries: int,
+) -> bool:
     """
     Makes a data request to the RedVox data_server.
     :param out_dir: The output directory to store downloaded files.
-    :param protocol: One of either https or http.
-    :param host: The host of the data server.
-    :param port: The port of the data server.
-    :param email: The email address of the redvox.io user.
-    :param password: The password of the redvox.io user.
+    :param redvox_config: An instance of the RedVox cloud configuration.
     :param req_start_s: The request start time as seconds since the epoch.
     :param req_end_s: The request end time as seconds since the epoch.
     :param redvox_ids: A list of RedVox ids.
     :param api_type: The API level to request data for (can be 900, 1000, or both)
     :param retries: The number of retries to perform on failed downloads.
-    :param secret_token: A shared secret token required for accessing the data service.
     :return: True if this succeeds, False otherwise.
     """
-    api_conf: cloud_api.ApiConfig = cloud_api.ApiConfig(protocol,
-                                                        host,
-                                                        port)
-    client: cloud_client.CloudClient = cloud_client.CloudClient(email,
-                                                                password,
-                                                                api_conf=api_conf,
-                                                                secret_token=secret_token)
-    data_resp: data_api.DataRangeResp = client.request_data_range(req_start_s, req_end_s, redvox_ids, api_type)
+    client: cloud_client.CloudClient = cloud_client.CloudClient(redvox_config)
+    data_resp: data_api.DataRangeResp = client.request_data_range(
+        req_start_s, req_end_s, redvox_ids, api_type
+    )
     client.close()
 
     if len(data_resp.signed_urls) == 0:
