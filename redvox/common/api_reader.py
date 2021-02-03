003e7e40f02dc61b11f6c919807d33177569fbad
tyler <tyler@isla.hawaii.edu>
2021-02-02 16:56:05 -1000

    fixed timing correction while getting files

--- redvox/common/api_reader.py
+++ redvox/common/api_reader.py
@@ -105,55 +105,14 @@ class ApiReader:
         # timing calibration on app set up/background process
         # phone can store its own drift model; send the critical values to the server
         # model confidence metric
-        index = self._apply_filter()
-        result_index = io.Index()
-        for station_id in self.filter.station_ids:
-            checked_index = self._check_station_stats(index.get_station_id(station_id))
-            result_index.append(checked_index.entries)
-        # todo: file_stats has all the stuff we need to do the thing
-        # todo: consider adding offset and packet length to the start time to get the partial first packet
-        return result_index
-
-    def _check_station_stats(self, index: io.Index) -> io.Index:
-        """
-        recursively check the index's values against the request filter; if index has enough information in it,
-        return it, otherwise search for more data.
-        :param index: index to check
-        :return: Index that includes as much information as possible that fits the request
-        """
-        stats = fs.extract_stats(index)
-        station_id = stats[0].station_id
-        # get the model and calculate the revised start and end offsets
-        # model = create_model_function
-        avg_offset = timedelta(microseconds=np.min([st.offset for st in stats if st.offset is not None]))
-        # revise packet's times to real times and compare to requested values
-        revised_start = stats[0].packet_start_dt + avg_offset  # model.revised_start_offset
-        revised_end = stats[-1].packet_start_dt + stats[-1].packet_duration + avg_offset  # model.revised_end_offset
-        # if our filtered files encompass the request even when the packet times are updated, return the index
-        if revised_start <= self.filter.start_dt and revised_end >= self.filter.end_dt:
-            return index
-        # we have to update our filter to get more information
-        new_filter = io.ReadFilter(station_ids={station_id}, extensions=self.filter.extensions,
-                                   api_versions=self.filter.api_versions)
-        # add an extra packet to the start value to ensure we don't miss any data
-        revised_start -= stats[0].packet_duration
-        # reduce the revised start value until it's less than the requested start value
-        while revised_start >= self.filter.start_dt:
-            revised_start -= stats[0].packet_duration
-        while revised_end <= self.filter.end_dt:
-            revised_end += stats[-1].packet_duration
-        # remove the offset so that you have the correct packet times to search for
-        new_filter = new_filter.with_start_dt(revised_start - avg_offset)\
-            .with_start_dt_buf(stats[0].packet_duration)\
-            .with_end_dt(revised_end - avg_offset)
-        # get information
-        new_index = self._apply_filter(new_filter)
-        new_stats = fs.extract_stats(new_index)
-        # compare start and end time of station vs start and end time of stats; if they match, return new result
-        if stats[0].packet_start_dt == new_stats[0].packet_start_dt \
-                and stats[-1].packet_start_dt == new_stats[-1].packet_start_dt:
-            return new_index
-        return self._check_station_stats(new_index)
+        index = io.Index()
+        all_index = self._apply_filter()
+        for station_id in all_index.summarize().station_ids():
+            station_filter = self.filter.clone()
+            station_filter = station_filter.with_station_ids({station_id})
+            checked_index = self._check_station_stats(station_filter)
+            index.append(checked_index.entries)
+        return index
 
     def _apply_filter(self, reader_filter: Optional[io.ReadFilter] = None) -> io.Index:
         """
@@ -168,6 +127,56 @@ class ApiReader:
         else:
             index = io.index_unstructured(self.base_dir, reader_filter)
         return index
+
+    def _check_station_stats(self, request_filter: io.ReadFilter) -> io.Index:
+        """
+        recursively check the filter's results; if resulting index has enough information in it,
+        return it, otherwise search for more data.
+        The filter should only request one station
+        :param request_filter: filter representing the requested information
+        :return: Index that includes as much information as possible that fits the request
+        """
+        index = self._apply_filter(request_filter)
+        if len(index.entries) < 1:
+            return index
+        index.sort()
+        stats = fs.extract_stats(index)
+        if stats[0].packet_duration == 0.0 or not stats[0].packet_duration:
+            return index
+        # get the model and calculate the revised start and end offsets
+        # model = create_model_function
+        latencies = [st.latency for st in stats if st.latency is not None and not np.isnan(st.latency)]
+        # no latencies means no correction means we're done
+        if len(latencies) < 1:
+            return index
+        i = np.argwhere([st.latency for st in stats] == np.min(latencies))[0][0]
+        avg_offset = timedelta(microseconds=stats[i].offset)
+        # revise packet's times to real times and compare to requested values
+        revised_start = stats[0].packet_start_dt + avg_offset  # model.revised_start_offset
+        revised_end = stats[-1].packet_start_dt + stats[-1].packet_duration + avg_offset  # model.revised_end_offset
+        if not request_filter.start_dt:
+            request_filter = request_filter.with_start_dt(revised_start)
+        if not request_filter.end_dt:
+            request_filter = request_filter.with_end_dt(revised_end)
+        # if our filtered files encompass the request even when the packet times are updated, return the index
+        if (not self.filter.start_dt or revised_start <= self.filter.start_dt) \
+                and (not self.filter.end_dt or revised_end >= self.filter.end_dt):
+            return index
+        # we have to update our filter to get more information
+        new_filter = request_filter.clone()
+        # add an extra packet to the filter times value to ensure we don't miss any data
+        revised_start = request_filter.start_dt - stats[0].packet_duration
+        revised_end = request_filter.end_dt + stats[-1].packet_duration
+        # remove the offset so that you have the correct packet times to search for
+        new_filter = new_filter.with_start_dt(revised_start - avg_offset) \
+            .with_start_dt_buf(stats[0].packet_duration) \
+            .with_end_dt(revised_end - avg_offset)
+        idx = self._apply_filter(new_filter)
+        sts = fs.extract_stats(idx)
+        if (revised_start <= self.filter.start_dt or sts[0].packet_start_dt == stats[0].packet_start_dt)\
+                and (revised_end >= self.filter.end_dt or sts[-1].packet_start_dt == stats[0].packet_start_dt):
+            return idx
+        return self._check_station_stats(new_filter)
 
     def read_files(self) -> Dict[str, List[WrappedRedvoxPacketM]]:
         """
