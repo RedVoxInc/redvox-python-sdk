dc6ab8e8bc1ce5f75cbe755999395076a3b80e20
opq <achriste@hawaii.edu>
2021-03-03 10:56:48 -1000

    Update api doc generation script.

--- scripts/generate-api-documentation.sh
+++ scripts/generate-api-documentation.sh
@@ -1,23 +1,14 @@
 #!/usr/bin/env bash
 
-
 cd ..
-
-VERSION=${1}
 
 if ! [[ -x "$(command -v pdoc3)" ]]; then
   echo 'Error: pdoc3 is not installed.' >&2
   exit 1
 fi
 
-if [[ -z ${VERSION} ]]; then
-    echo "missing required version"
-    exit 1
-fi
-
 set -o nounset
 set -o errexit
 set -o xtrace
 
-
-pdoc3 redvox --overwrite --html --html-dir docs/v${VERSION}/api_docs -c show_type_annotations=True
+pdoc3 redvox --overwrite --html --html-dir docs/api_docs -c show_type_annotations=True
