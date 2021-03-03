02edcf57cb1f027c6f1cec0ca69c0b3318b09c92
opq <achriste@hawaii.edu>
2021-03-02 15:18:55 -1000

    Documentation.

--- redvox/common/parallel_utils.py
+++ redvox/common/parallel_utils.py
@@ -1,3 +1,7 @@
+"""
+This module provides a lazy singleton of a process pool.
+"""
+
 from multiprocessing import cpu_count, Pool
 import multiprocessing.pool
 from typing import Optional
@@ -6,6 +10,9 @@ __pool: Optional[multiprocessing.pool.Pool] = None
 
 
 def pool() -> multiprocessing.pool.Pool:
+    """
+    :return: An instance of a process pool if one exists, otherwise, one is created and returned.
+    """
     global __pool
     if __pool is None:
         __pool = Pool(cpu_count())
