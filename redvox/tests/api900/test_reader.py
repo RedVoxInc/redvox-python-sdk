6e5144db996980b5c469e1cd602058496d3e1204
Anthony Christe <anthony.christe@gmail.com>
2021-03-25 15:09:42 -1000

    Update reader tests to use the correct slashs for paths for a given platform.

--- redvox/tests/api900/test_reader.py
+++ redvox/tests/api900/test_reader.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 import redvox.api900.reader as reader
@@ -26,46 +27,46 @@ class TestReader(unittest.TestCase):
         self.assertFalse(reader._is_valid_redvox_filename("1637681006_1546631601587"))
 
     def test_is_path_in_set(self):
-        self.assertTrue(reader._is_path_in_set("/1637681006_1546631601587.rdvxz",
+        self.assertTrue(reader._is_path_in_set(f"{os.sep}1637681006_1546631601587.rdvxz",
                                                1546631601 - 1,
                                                1546631601 + 1,
                                                {"1637681006"}))
-        self.assertFalse(reader._is_path_in_set("/1637681007_1546631601587.rdvxz",
+        self.assertFalse(reader._is_path_in_set(f"{os.sep}1637681007_1546631601587.rdvxz",
                                                 1546631601 - 1,
                                                 1546631601 + 1,
                                                 {"1637681006"}))
-        self.assertTrue(reader._is_path_in_set("/0000000001_1000000000000.rdvxz",
+        self.assertTrue(reader._is_path_in_set(f"{os.sep}0000000001_1000000000000.rdvxz",
                                                1000000000 - 1,
                                                1000000000 + 1,
                                                {"0000000001", "0000000002"}))
-        self.assertTrue(reader._is_path_in_set("/0000000002_1000000000000.rdvxz",
+        self.assertTrue(reader._is_path_in_set(f"{os.sep}0000000002_1000000000000.rdvxz",
                                                1000000000 - 1,
                                                1000000000 + 1,
                                                {"0000000001", "0000000002"}))
-        self.assertFalse(reader._is_path_in_set("/0000000003_1000000000000.rdvxz",
+        self.assertFalse(reader._is_path_in_set(f"{os.sep}0000000003_1000000000000.rdvxz",
                                                 1000000000 - 1,
                                                 1000000000 + 1,
                                                 {"0000000001", "0000000002"}))
-        self.assertTrue(reader._is_path_in_set("/0000000001_1000000000000.rdvxz",
+        self.assertTrue(reader._is_path_in_set(f"{os.sep}0000000001_1000000000000.rdvxz",
                                                1000000000,
                                                1000000000 + 1,
                                                {"0000000001", "0000000002"}))
-        self.assertTrue(reader._is_path_in_set("/0000000001_1000000000000.rdvxz",
+        self.assertTrue(reader._is_path_in_set(f"{os.sep}0000000001_1000000000000.rdvxz",
                                                1000000000 - 1,
                                                1000000000,
                                                {"0000000001", "0000000002"}))
 
-        self.assertTrue(reader._is_path_in_set("/0000000001_1000000000000.rdvxz",
+        self.assertTrue(reader._is_path_in_set(f"{os.sep}0000000001_1000000000000.rdvxz",
                                                1000000000,
                                                1000000000,
                                                {"0000000001", "0000000002"}))
 
-        self.assertFalse(reader._is_path_in_set("/0000000001_1000000000000.rdvxz",
+        self.assertFalse(reader._is_path_in_set(f"{os.sep}0000000001_1000000000000.rdvxz",
                                                 2000000000,
                                                 3000000000,
                                                 {"0000000001", "0000000002"}))
 
-        self.assertFalse(reader._is_path_in_set("/0000000001_4000000000000.rdvxz",
+        self.assertFalse(reader._is_path_in_set(f"{os.sep}0000000001_4000000000000.rdvxz",
                                                 1000000000,
                                                 3000000000,
                                                 {"0000000001", "0000000002"}))
