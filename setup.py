0c2127cb9ab914f23a5a2187767dcbeeb3ed83d9
Anthony Christe <achriste@redvoxsound.com>
2021-05-04 14:34:48 -1000

    bump native version.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(name=redvox.NAME,
       install_requires=requirements,
       extras_require={
           "GUI":  ["PySide6>=6.0.2", "matplotlib>=3.2.2"],
-          "native": ["redvox_native>=0.1.0"]
+          "native": ["redvox_native>=0.1.1"]
       },
       entry_points={
             'console_scripts': [
