1d7a85615bb26e12485ead2fb5d8ea6726185ff7
yusukemh <yusukemh@hawaii.edu>
2021-11-10 12:24:16 -1000

    throws error

--- redvox/tests/test_issue360.py
+++ redvox/tests/test_issue360.py
@@ -1,41 +1,19 @@
-from redvox.common.api_reader import ApiReader
-from redvox.common.data_window import DataWindow
 import shutil
-from time import time
 import os
-from os import listdir
-from os.path import isfile, join
-import numpy as np
 from pathlib import Path
-
 from redvox.common import data_window_wpa as dwpa
-from redvox.common import data_window_io as dw_io
 
 
 def main():
-    """
-    input_dir = "/Users/yusukehatanaka/Desktop/DATA/20200222_GoodComms/"
-    dw = DataWindow(
-        input_dir=input_dir,
-    )
-    stations = dw.station_ids
-    print(stations)
-
-    for station in stations:
-        print(f"processing {station}")
-        _ = DataWindow(
-            input_dir=input_dir,
-            station_ids=[station]
-        )
-        print('==================================')
-    """
     # ['1637199002', '1637610029',
     # '1637662003', '1637610028',
     # '1637110701', '1637199003']
-    path = "/Users/yusukehatanaka/Desktop/DATA/20200222_GoodComms/"
+    # path = "/Users/yusukehatanaka/Desktop/DATA/20200222_GoodComms/"
+    # path = "/Users/yusukehatanaka/Desktop/DATA/20201027_SDK_Example/"
+    path = "/Users/yusukehatanaka/Desktop/DATA/20180824_Hurricane_Lane"
+
     save_path = "/Users/yusukehatanaka/Desktop/DATA/issue_360/"
     if os.path.exists(save_path):
-        # os.rmdir(save_path)
         shutil.rmtree(save_path)
     os.mkdir(save_path)
     dw_config = dwpa.DataWindowConfigWpa(
@@ -50,14 +28,17 @@ def main():
     )
     drws.save()
     file_bytes = sum(file.stat().st_size for file in Path(save_path).rglob('*'))
-    print(file_bytes)
+    # print(file_bytes)
 
-    print(drws.station_ids())
+    # print(drws.station_ids())
     for station in drws.station_ids():
         sample_rate = drws.get_station(station)[0].audio_sample_rate_nominal_hz()
-        print(f"station: {station}, SR: {sample_rate}")
-
-    print(folder_size(save_path))
+        folder_per_station = [filename for filename in os.listdir(save_path) if filename.startswith(f"{station}")]
+        print(
+            f"station_id: {station}, " +
+            f"num_bytes: {folder_size(save_path + folder_per_station[0])/1000} KB, " +
+            f"SR: {sample_rate}"
+        )
 
 
 def folder_size(path='.'):
